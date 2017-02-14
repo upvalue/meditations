96cfecda06c17307621aea107ab4985f0f73340e
Phil <phil@ioddly.com>
2017-02-14 12:59:13 -0600

    Include fonts and icons in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,12 @@ const config = {
     habits: ["./assets/webpack-entry-habits.js"],
     journal: ["./assets/webpack-entry-journal.js"],
   },
-  //entry: './assets/webpack-entry.js',
+
   output: {
     path: path.resolve(__dirname, 'assets'),
     filename: 'webpack-bundle-[name].js'
   },
+
   resolve: {
     alias: {
       jquery: path.resolve(__dirname, 'assets/vendor/jquery/dist/jquery.min'),
@@ -19,22 +20,24 @@ const config = {
       riot: path.resolve(__dirname, "assets/vendor/riot/riot+compiler.min"),
     },
   },
+
   externals: ['window'],
+
   plugins: [
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     new webpack.ProvidePlugin({
       "Tether": "Tether", // required for bootstrap
     })
   ],
+
   /* Compile riot .tag files */
   module: {
     rules: [
       { test: /\.css$/,
         loader: 'style-loader!css-loader',
       },
-      { test: /\.(woff|png|ttf|svg|eot)$/,
-        loader: 'file-loader',
-        options: { name: 'assets/{hash}.{ext}' } },
+      { test: /\.(woff|png|ttf|svg|eot|woff2)$/,
+        loader: 'file-loader?name=copied/[hash].[ext]&context=./assets&publicPath=assets/' },
       { enforce: 'pre', test: /\.tag$/, exclude: /assets\/vendor/, loader: 'riotjs-loader' },
     ],
   },
