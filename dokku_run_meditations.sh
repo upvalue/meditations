6553d56b74e1eb579d6682d3b5e62d03bc25d167
Phil <phil@ioddly.com>
2017-03-08 17:42:05 -0600

    Remove webpack option from dokku

--- dokku_run_meditations.sh
+++ dokku_run_meditations.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./bin/meditations serve --migrate --port $PORT --tutorial --message "This is a demo, reset every hour. <a href=\"https://ioddly.com\">Like what you see?</a>" --webpack
+./bin/meditations serve --migrate --port $PORT --tutorial --message "This is a demo, reset every hour. <a href=\"https://ioddly.com\">Like what you see?</a>" 
