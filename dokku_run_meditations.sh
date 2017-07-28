53ce38495c80fd478cde4ee389b5e556fe720de5
Phil <phil@upvalue.io>
2017-07-28 16:12:32 -0500

    Attempt to fix CircleCI 2

--- dokku_run_meditations.sh
+++ dokku_run_meditations.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+./node_modules/.bin/webpack -p
 ./bin/meditations serve --migrate --port $PORT --message "This is a demo, reset every hour. <a href=\"https://ioddly.com\">Like what you see?</a>" 
