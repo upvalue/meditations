7cbbc8d9db0c067ede2494f0084967fc1e4705e7
Phil <phil@upvalue.io>
2018-11-20 22:23:26 -0600

    react-beautiful-dnd works #35

--- frontend-ng/src/habits/HabitsPage.tsx
+++ frontend-ng/src/habits/HabitsPage.tsx
@@ -15,26 +15,37 @@ export interface HabitsPageProps extends RouteComponentProps { }
 
 const Task = (props: any) => {
   return (
-    <div className="Task p2 mb1">
-      <div className="flex items-center justify-between">
-        {/*<MdCheckCircle color={"green"} />
+    <Draggable
+      draggableId={props.taskId}
+      index={props.index}
+      type="TASK"
+    >
+      {(provided, snapshot) => (
+        <div className="Task mb1" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+          <div className="flex items-center justify-between">
+            {/*<MdCheckCircle color={"green"} />
         <Button className="ml2" style={{ display: 'inline' }}>
           &nbsp;{props.children}
   </Button>*/}
-        <Button draggable={true} className="" style={{ display: 'inline' }}>
-          {props.children}
-        </Button>
+            <Button
+              className="" style={{ display: 'inline' }}
+            >
+              {props.children}
+            </Button>
 
-        <div className="mr1">
-          <MdCheckCircle />
+            <div className="mr1">
+              <MdCheckCircle />
+            </div>
+          </div>
+          {
+            props.comment &&
+            <div className="Comment mt2 ml3 pb2">
+              This is what a comment might look like. If it were really, really, really fwiggin long
         </div>
-      </div>
-      {props.comment &&
-        <div contentEditable={true} className="Comment mt3 ml3 pb3">
-          This is what a comment might look like. If it were really, really, really fwiggin long
-        </div>
-      }
-    </div>
+          }
+        </div >
+      )}
+    </Draggable>
   );
 }
 
@@ -51,10 +62,25 @@ const Scope = (props: any) => {
           className="Comment mt3 ml3 mb1">This is a comment about ultimate power</p>
   </div>*/}
 
-      <Task>Exercise 1/10 (10%)</Task>
-      <Task>Diet 15/18 (83%)</Task>
-      <Task>Feed the lawn</Task>
-      <Task comment={true}>Mow the cat</Task>
+      <Droppable
+        droppableId={props.name || 'November'}
+        type="LIST"
+      >
+        {(provided, snapshot) => (
+          <>
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              <Task taskId={`${props.name}-0`} index={0}>Exercise 1/10 (10%)</Task>
+              <Task taskId={`${props.name}-1`} index={1}> Diet 15/18 (83%)</Task>
+              <Task taskId={`${props.name}-2`} index={2} comment={true}>Feed the lawn</Task>
+              <Task taskId={`${props.name}-3`} index={3}>Mow the cat</Task>
+              <Task taskId={`${props.name}-4`} index={4}>Something</Task>
+              <Task taskId={`${props.name}-5`} index={5}>Whatever</Task>
+              <Task taskId={`${props.name}-6`} index={6}>Thing</Task>
+              {provided.placeholder}
+            </div>
+          </>
+        )}
+      </Droppable>
 
       <div>
         <Button minimal={true}>
@@ -69,44 +95,45 @@ const Scope = (props: any) => {
 export const HabitsPage = (props: HabitsPageProps) => {
   return (
     <>
-      <View flex="flex-column" style={{ width: '26em' }}>
-        <View className="sidebar" flex={['flex-column', 'flex-auto']}>
-          <div className="flex justify-between items-center">
-            <div className="flex">
-              {/* What this needs is a double arrow like a VHS rewind */}
-              <HeaderIconButton icon={MdChevronLeft} />
-              <HeaderIconButton icon={MdArrowBack} />
-            </div>
+      <DragDropContext
+        onDragEnd={e => console.log('hot dog')}
+      >
+        <View flex="flex-column" style={{ width: '26em', minWidth: '26em' }}>
+          <View className="sidebar" flex={['flex-column', 'flex-auto']}>
+            <div className="flex justify-between items-center">
+              <div className="flex">
+                {/* What this needs is a double arrow like a VHS rewind */}
+                <HeaderIconButton icon={MdChevronLeft} />
+                <HeaderIconButton icon={MdArrowBack} />
+              </div>
 
-            <div
-              className="flex-auto"
-              style={{ textAlign: 'center' }}
-            >
-              <h3 className="m0">
-                December 2018
+              <div
+                className="flex-auto"
+                style={{ textAlign: 'center' }}
+              >
+                <h3 className="m0">
+                  December 2018
               </h3>
+              </div>
+
+              <div className="flex">
+                <HeaderIconButton icon={MdArrowForward} />
+                <HeaderIconButton icon={MdChevronRight} />
+              </div>
+
             </div>
-
-            <div className="flex">
-              <HeaderIconButton icon={MdArrowForward} />
-              <HeaderIconButton icon={MdChevronRight} />
-            </div>
-
-          </div>
-          <span>some other controls or whatever</span>
+            <span>some other controls or whatever</span>
+          </View>
         </View>
-      </View>
 
-      <main className="ml3 pt3 flex-auto">
-        <View flex="flex-auto">
-          <Scope className="mr2" style={{ width: '33%' }} />
-          <Scope name="2018" className="mr2" style={{ width: '33%' }} />
-          <Scope name="Project" className="mr2" style={{ width: '33%' }} />
-        </View>
-        <View>
-
-        </View>
-      </main>
+        <main className="ml3 pt3 flex-auto">
+          <View flex="flex-auto">
+            <Scope className="mr2" style={{ width: '33%' }} />
+            <Scope name="2018" className="mr2" style={{ width: '33%' }} />
+            <Scope name="Project" className="mr2" style={{ width: '33%' }} />
+          </View>
+        </main>
+      </DragDropContext>
     </>
   );
 };
