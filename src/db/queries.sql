0d4d1a7160952767df9f77a8f7fc68b6dcbc6b65
Phil <phil@upvalue.io>
2024-05-29 13:39:00 -0700

    Added update, delete, and insert queries for documents and tags.

--- src/db/queries.sql
+++ src/db/queries.sql
@@ -4,3 +4,16 @@ SELECT 1 as one;
 /* @name selectTwo */
 SELECT 2 as two;
 
+/* @name updateDocById */
+UPDATE docs
+SET body = :body, revision = :revision, updated_at = now()
+WHERE id = :id;
+
+/* @name dropDocTags */
+DELETE FROM doc_tags
+WHERE doc_id = :id;
+
+/* @name insertDocTags */
+INSERT INTO doc_tags (doc_id, doc_location, tag_name)
+VALUES (:doc_id, :doc_location, :tag_name);
+
