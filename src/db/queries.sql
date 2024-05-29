c93c7eb9f63e55314d38193f9deb1ef83d84263c
Phil <phil@upvalue.io>
2024-05-29 13:39:12 -0700

    Updated queries for updating and managing document tags.

--- src/db/queries.sql
+++ src/db/queries.sql
@@ -17,3 +17,16 @@ WHERE doc_id = :id;
 INSERT INTO doc_tags (doc_id, doc_location, tag_name)
 VALUES (:doc_id, :doc_location, :tag_name);
 
+/* @name updateDocById */
+UPDATE docs
+SET body = :body, revision = :revision, updated_at = now()
+WHERE id = :id;
+
+/* @name dropDocTags */
+DELETE FROM doc_tags
+WHERE doc_id = :id;
+
+/* @name insertDocTags */
+INSERT INTO doc_tags (doc_id, doc_location, tag_name)
+VALUES (:doc_id, :doc_location, :tag_name);
+
